00bd1650b6e3a88c46a7575384a317ef5a403919
yanzhenhua01 <yanzhenhua01@zuoyebang.com>
2022-04-23 20:01:07 +0800

    some questions

--- LeetCode/T700+/T_001_L704二分查找_test.go
+++ LeetCode/T700+/T_001_L704二分查找_test.go
@@ -4,7 +4,51 @@ import (
 	"testing"
 )
 
-func TestSearch(t *testing.T) {
+func Test_searchV2(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "t1", args: args{nums: []int{-1, 0, 3, 5, 9, 12}, target: 9}, want: 4},
+		{name: "t2", args: args{nums: []int{-1, 0, 3, 5, 9, 12}, target: 2}, want: -1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := searchV2(tt.args.nums, tt.args.target); got != tt.want {
+				t.Errorf("searchV2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_search(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantRet int
+	}{
+		{name: "t1", args: args{nums: []int{-1, 0, 3, 5, 9, 12}, target: 9}, wantRet: 4},
+		{name: "t2", args: args{nums: []int{-1, 0, 3, 5, 9, 12}, target: 2}, wantRet: -1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotRet := search(tt.args.nums, tt.args.target); gotRet != tt.wantRet {
+				t.Errorf("search() = %v, want %v", gotRet, tt.wantRet)
+			}
+		})
+	}
+}
+
+func Test_searchRepeat1(t *testing.T) {
 	type args struct {
 		nums   []int
 		target int
@@ -20,7 +64,7 @@ func TestSearch(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if gotRet := searchRepeat1(tt.args.nums, tt.args.target); gotRet != tt.wantRet {
-				t.Errorf("Search() = %v, want %v", gotRet, tt.wantRet)
+				t.Errorf("searchRepeat1() = %v, want %v", gotRet, tt.wantRet)
 			}
 		})
 	}
