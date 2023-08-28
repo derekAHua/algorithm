498267f5efbaaa41c6e69056dbf66f3d3159d6f4
yanzhenhua01 <16600240774@163.com>
2023-08-28 21:14:03 +0800

    dp-子序列

--- LeetCode/T500+/L516.最长回文子序列.go
+++ LeetCode/T500+/L516.最长回文子序列.go
@@ -2,7 +2,35 @@ package T500_
 
 // https://leetcode.cn/problems/longest-palindromic-subsequence/
 
-func longestPalindromeSubseq(s string) int {
+func longestPalindromeSubseq(s string) (ret int) {
+	size := len(s)
+	max := func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+
+	dp := make([][]int, size)
+	for i := 0; i < size; i++ {
+		dp[i] = make([]int, size)
+		dp[i][i] = 1
+	}
+	ret = 1
+	for i := len(s) - 1; i >= 0; i-- {
+		for r := i + 1; r < len(s); r++ {
+			if s[i] == s[r] {
+				dp[i][r] = dp[i+1][r-1] + 2
+			} else {
+				dp[i][r] = max(dp[i][r-1], dp[i+1][r])
+			}
+		}
+	}
+
+	return dp[0][size-1]
+}
+
+func longestPalindromeSubseqR1(s string) int {
 	size := len(s)
 	max := func(a, b int) int {
 		if a > b {
--- LeetCode/T600+/L647. 回文子串.go
+++ LeetCode/T600+/L647. 回文子串.go
@@ -8,6 +8,34 @@ func countSubstrings(s string) (ret int) {
 		dp[i] = make([]bool, len(s))
 	}
 
+	for l := len(s) - 1; l >= 0; l-- {
+		for r := l; r < len(s); r++ {
+			if s[l] != s[r] {
+				continue
+			}
+
+			if r-l <= 1 {
+				ret++
+				dp[l][r] = true
+				continue
+			}
+
+			if dp[l+1][r-1] {
+				ret++
+				dp[l][r] = true
+			}
+		}
+	}
+
+	return
+}
+
+func countSubstringsR1(s string) (ret int) {
+	dp := make([][]bool, len(s))
+	for i := range dp {
+		dp[i] = make([]bool, len(s))
+	}
+
 	for i := len(s) - 1; i >= 0; i-- {
 		for j := i; j < len(s); j++ {
 			if s[i] != s[j] {
